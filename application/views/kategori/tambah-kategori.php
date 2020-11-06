f9c66cfa20bdc05195af1b22aa768b1f2a4c94c2
AndikaPermanaSidiq01 <andikapermanasidiq00@gmail.com>
2020-11-06 23:38:51 +0700

    Selesaigit statusgit statusgit status

--- application/views/kategori/tambah-kategori.php
+++ application/views/kategori/tambah-kategori.php
@@ -1,7 +1,11 @@
 <div class="container mt-5">
   <div class="row">
     <div class="col-12">
-      <h4 class="text-bold text-uppercase mb-3">Tambah Kategori</h4>
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h4 class="text-bold text-uppercase my-auto">Tambah Kategori</h4>
+
+        <a href="<?= base_url('kategori'); ?>" class="btn btn-primary">Kembali</a>
+      </div>
       <div class="card custom-card">
         <div class="card-body">
           <form action="<?= base_url('kategori/tambah'); ?>" method="POST">
